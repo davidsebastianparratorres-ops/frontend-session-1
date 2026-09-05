67f54b946b3c603f3ace95f70dbed893c1ee08b4
davidsebastianparratorres-ops <davidsebastianparratorres@gmail.com>
2026-09-04 20:48:35 -0500

    feat: estilos de css

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+console.log("Accedí a JS");
+
+function saludoFuncion() {
+    console.log("hola desde la función")
+}
+
+const SALUDO_FLECHA =()=>{
+    console.log("hola desde la función flecha")
+}
+
+const SALUDO_DOS=function() {
+    console.log("hola desde la funcion anonima")
+}
